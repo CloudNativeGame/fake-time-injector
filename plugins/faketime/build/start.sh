ffa52b3f6cabb19b10555e5333b9e8629601f37d
“skkkkkkk” <sk01199367@alibaba-inc.com>
2023-06-20 16:39:16 +0800

    fix libfaketime

--- plugins/faketime/build/start.sh
+++ plugins/faketime/build/start.sh
@@ -1,10 +1,11 @@
-#! /bin/bash
-#sleep 2
-#sp_pid=`ps ax|grep $modify_process_name|grep -v grep|grep -v /bin/sh|awk '{print $1}'`
-#./bin/watchmaker -pid $sp_pid -sec_delta $delay_second -nsec_delta 0 -clk_ids "CLOCK_REALTIME,CLOCK_MONOTONIC"
+#!/bin/bash
+
 array=(`echo $modify_process_name | tr ',' ' '`)
 for modify_process in ${array[@]}
 do
   sp_pid=`ps ax|grep $modify_process|grep -v grep|grep -v /bin/sh|awk '{print $1}'`
-  ./bin/watchmaker -pid $sp_pid -sec_delta $delay_second -nsec_delta 0 -clk_ids "CLOCK_REALTIME,CLOCK_MONOTONIC"
+  if [ -n "$sp_pid" ]
+  then
+    ./bin/watchmaker -pid $sp_pid -sec_delta $delay_second -nsec_delta 0 -clk_ids "CLOCK_REALTIME,CLOCK_MONOTONIC"
+  fi
 done
